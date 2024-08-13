a00b52cdaee267329868492706cc7e949e0c7e52
bobo459 <awd458@naver.com>
2024-08-13 17:23:30 +0900

    v0813

--- src/main/java/DW/PocatRush/controller/PostController.java
+++ src/main/java/DW/PocatRush/controller/PostController.java
@@ -1,10 +1,19 @@
 package DW.PocatRush.controller;
 
+import DW.PocatRush.model.Post;
 import DW.PocatRush.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("api/post")
 public class PostController {
 
     PostService postService;
@@ -13,4 +22,16 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
+    @GetMapping("/freeboard")
+    public ResponseEntity<List<Post>> getPostByFreeboard() {
+        long boardId = 1; // 자유게시판 직접 불러오기
+        return new ResponseEntity<>(postService.getPostByBoardId(boardId), HttpStatus.OK);
+    }
+    @GetMapping("/whatsnew")
+    public ResponseEntity<List<Post>> getPostByWhatsnew() {
+        long boardId = 2; // 공지사항 직접 불러오기
+        return new ResponseEntity<>(postService.getPostByBoardId(boardId), HttpStatus.OK);
+    }
+
 }
--- src/main/java/DW/PocatRush/service/PostService.java
+++ src/main/java/DW/PocatRush/service/PostService.java
@@ -1,9 +1,15 @@
 package DW.PocatRush.service;
 
+import DW.PocatRush.exception.ResourceNotFoundException;
+import DW.PocatRush.model.Post;
 import DW.PocatRush.repository.PostRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -15,4 +21,15 @@ public class PostService {
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
+
+
+
+    public List<Post> getPostByBoardId (long boardId){
+        return postRepository.findAll()
+                .stream().filter((post -> post.getBoard().getBoardId() == boardId))
+                .collect(Collectors.toList())
+                .reversed();
+    }
+
+
 }
